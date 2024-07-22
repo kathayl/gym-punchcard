c9e1ccc4b0ac5234831a030ee123c710c417df37
kathayl <kathyyliao@gmail.com>
2024-07-21 22:04:11 -0700

    add cookies so different users can use

--- scripts.js
+++ scripts.js
@@ -331,3 +331,41 @@ const activityIcons = {
     }
   }
   
+ // Generate a unique identifier
+function generateUUID() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  }
+  
+  // Set a cookie
+  function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+      expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+  }
+  
+  // Get a cookie
+  function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+  }
+  
+  // Check if user ID exists, if not, generate one
+  let userId = getCookie('userId');
+  if (!userId) {
+    userId = generateUUID();
+    setCookie('userId', userId, 365); // Set cookie for 1 year
+  }
+  
